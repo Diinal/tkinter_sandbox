59888736d7c79bda5cd17444b8b4974d3c41f8eb
Diinal <dloshmanov97@yandex.ru>
2019-09-27 16:11:29 +0300

    fm added

--- FM.py
+++ FM.py
@@ -43,27 +43,13 @@ def signal_freq_change(event=None):
     s_frq = int(signal_freq.get())
 
 def signal_amp_change(event=None):
-    global s_amp, c_amp, carry_amp
+    global s_amp, c_amp
     s_amp = int(signal_amp.get())
-    carry_amp.set(s_amp*2)
-    c_amp = s_amp*2
 
 #carry
 def carry_freq_change(event=None):
     global c_frq
     c_frq = int(carry_freq.get())
-
-def carry_amp_change(event=None):
-    global c_amp
-    c_amp = int(carry_amp.get())
-
-def scale_up(event=None):
-    prev = signal_lvl.get()
-    signal_lvl.set(prev+1)
-
-def scale_down(event=None):
-    prev = signal_lvl.get()
-    signal_lvl.set(prev-1)
 
 plot_container = tk.LabelFrame(root, text = '', height = 960, width = 1280, font = font_, bg = 'white')
 plot_container.grid(row = 0, column = 0, rowspan = 9, padx = 5, pady = 5)
@@ -71,21 +57,9 @@ plot_container.configure(background = background_, foreground = foreground_)
 
 #main label
 type_text = tk.StringVar()
-type_text.set('Амплитудная модуляция')
+type_text.set('Частотная модуляция')
 type_label = ttk.Label(plot_container, textvariable = type_text, font = font_, background = background_)
 type_label.grid(row = 0, column = 0, sticky = ('N'), padx = 2, pady = 5)
-
-#vert scale
-signal_lvl = tk.IntVar()
-signal_scale = tk.Scale(root, from_ = 100, to = -100, bg = 'white', length = 250, variable = signal_lvl)
-signal_scale.place(x = 25, y = 140)
-signal_lvl.set(25)
-
-up_button = tk.Button(root, text = '+', command = scale_up, font = font_, bg = 'white', width = 2)
-up_button.place(x = 25, y = 100)
-
-down_button = tk.Button(root, text = '-', command = scale_down, font = font_, bg = 'white', width = 2)
-down_button.place(x = 25, y = 395)
 
 #Signal frequency
 signal_freq_lbl = ttk.Label(root, text = 'Частота (fc)', font = font_)
@@ -115,21 +89,20 @@ carry_freq_box = tk.Spinbox(root, from_ = 10, to = 100, textvariable = carry_fre
 carry_freq_box.grid(row = 3, column = 2, sticky = ('E', 'W', 'S'), padx = 2, pady = 5)
 carry_freq_box.bind('<Return>', carry_freq_change)
 
-#Carrying amplitude
-carry_amp_lbl = ttk.Label(root, text = 'Амплитуда', font = font_)
-carry_amp_lbl.grid(row = 4, column = 1, sticky = ('E'), padx = (40, 5), pady = 5)
-carry_amp = tk.IntVar()
-carry_amp.set(50)
-carry_amp_box = tk.Spinbox(root, from_ = 5, to = 100, textvariable = carry_amp, font = font_, foreground = foreground_, command = carry_amp_change, increment = 5.0, state = 'disable')
-carry_amp_box.grid(row = 4, column = 2, sticky = ('E', 'W'), padx = 2, pady = 5)
-carry_amp_box.bind('<Return>', func)
+
+deviation = tk.StringVar()
+deviation_lbl = ttk.Label(root, text = 'Девиация', font = font_)
+deviation_input = ttk.Entry(root, textvariable = deviation, font = font_)
+deviation_lbl.place(x = 1270, y = 900)
+deviation_input.place(x = 1380, y = 900)
+deviation.set('6.0')
 
 #variables
 s_frq = int(signal_freq.get())
 s_amp = int(signal_amp.get())
 
 c_frq = int(carry_freq.get())
-c_amp = int(carry_amp.get())
+c_amp = 25
 #c_width = int(carry_width.get())/100
 
 #plotting
@@ -137,38 +110,27 @@ fig = plt.Figure(figsize=(12, 10))
 x = np.arange(0, 10, 0.01)
 
 def s_ani(i):
-    if (max(np.sin((x-i/50.0)*s_frq)*s_amp) + signal_lvl.get()) < 99 and (min(np.sin((x+i/50.0)*s_frq)*s_amp) + signal_lvl.get()) > -99:
-        s_line.set_ydata(np.sin((x-i/50.0)*s_frq)*s_amp + signal_lvl.get())
-    elif signal_lvl.get() > 0:
-        s_line.set_ydata(np.sin((x-i/50.0)*s_frq)*s_amp + 99 - s_amp)
-    else:
-        s_line.set_ydata(np.sin((x-i/50.0)*s_frq)*s_amp - 99 + s_amp)
+    s_line.set_ydata(np.sin((x-i/50.0)*s_frq)*s_amp)
     return s_line,
 
 def c_ani(i):
+    c_amp = signal_amp.get()
     c_line.set_ydata(np.sin((x-i/50.0)*c_frq)*c_amp)
     return c_line,
 
 def m_ani(i):
+    y_signal = np.sin((x-i/50.0)*s_frq)*s_amp
+    dev = 6
 
-    y_envelope_up = (np.sin((x-i/50.0)*s_frq)*s_amp)
-    y_envelope_down = (np.sin((x-i/50.0)*s_frq+np.pi)*s_amp)
+    if not deviation.get().isalpha() and deviation.get() != '':
+        dev = deviation.get().replace(',', '.')
+        dev = float(dev)
 
-    if (max(y_envelope_up) + signal_lvl.get()) < 99 and (min(y_envelope_up) + signal_lvl.get()) > -99:
-        y_envelope_up = y_envelope_up + signal_lvl.get()
-        y_envelope_down = y_envelope_down - signal_lvl.get()
-    elif signal_lvl.get() > 0:
-        y_envelope_up = y_envelope_up + 99 - s_amp
-        y_envelope_down = y_envelope_down - 99 + s_amp
-    else:
-        y_envelope_up = y_envelope_up - 99 + s_amp
-        y_envelope_down = y_envelope_down + 99 - s_amp
-    
-    cm_line.set_ydata(np.sin((x-i/50.0)*c_frq)*c_amp*(y_envelope_up/(s_amp*2)))
-    sm_line.set_ydata(y_envelope_up)
-    #sm_line.set_color('red')
-    spm_line.set_ydata(y_envelope_down)
-    return cm_line, sm_line, spm_line#, subsignal_label
+    c_amp = signal_amp.get()
+    y_carry = np.sin((x-i/50.0)*c_frq - dev*np.cos((x-i/50.0)*s_frq))*c_amp
+    cm_line.set_ydata(y_carry)
+    sm_line.set_ydata(y_signal)
+    return cm_line, sm_line
 
 def normalize(ax):
     ax.set_xlim(0, 10)
@@ -199,9 +161,9 @@ c_line, = c_ax.plot(x, y_carry)
 y_signal = (np.sin((x)*s_frq)*s_amp)+y_max
 y_max = max(y_signal)
 y_carry = y_carry*(y_signal/y_max)
-cm_line, = m_ax.plot(x, y_carry, 'k')
-spm_line, = m_ax.plot(x, y_signal, 'b', linewidth=2)
-sm_line, = m_ax.plot(x, y_signal, 'r', linewidth=2)
+cm_line, = m_ax.plot(x, y_carry, 'r')
+#spm_line, = m_ax.plot(x, y_signal, 'b', linewidth=2)
+sm_line, = m_ax.plot(x, y_signal, 'k', linewidth=0.5)
 
 s_ax.cla()
 c_ax.cla()
@@ -213,7 +175,7 @@ normalize(m_ax)
 
 s_ax.legend([s_line], ['Сигнал с fс'], loc = 'upper center', frameon=False)
 c_ax.legend([c_line], ['Несущее колебание c fн'], loc = 'upper center', frameon=False)
-m_ax.legend([cm_line, sm_line, spm_line], ['Амплитудно модулированный сигнал\n(модулированное по амплитуде колебание)', 'Верхняя огибающая', 'Нижняя огибающая'], ncol = 3, loc = 'upper center', frameon=False)
+m_ax.legend([cm_line, sm_line], ['Частотно модулированный сигнал\n(модулированное по частоте колебание)', 'Сигнал с fс'], ncol = 3, loc = 'upper center', frameon=False)
 #c_repeatable_point = int(250 * c_frq / 10)
 
 a1 = animation.FuncAnimation(fig, s_ani, np.arange(1, 315), interval=20, blit=True)
